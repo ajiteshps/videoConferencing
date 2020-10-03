5d8af2b1e3e921e6309be7a19cfe5744b92f36d3
Ajitesh <ajiteshps@gmail.com>
2020-10-03 21:44:08 +0530

    Inital Commit

--- scripts/server.js
+++ scripts/server.js
@@ -1,21 +1,23 @@
 // Simple Express server setup to serve the build output
-// Simple Express server setup to serve for local testing/dev API server
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+
 app.use(helmet());
 app.use(compression());
 
-const HOST = process.env.API_HOST || 'localhost';
-const PORT = process.env.API_PORT || 3002;
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
 
-app.get('/api/v1/endpoint', (req, res) => {
-    res.json({ success: true });
-});
+app.use(express.static(DIST_DIR));
+
 app.get('/roomIdGen', (req, res) => {
     res.json({ roomId: uuidV4() });
 });
@@ -31,8 +33,9 @@ io.on('connection', (socket) => {
     });
 });
 
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
 server.listen(PORT, () =>
-    console.log(
-        `✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`
-    )
+    console.log(`✅  Server started: http://${HOST}:${PORT}`)
 );
